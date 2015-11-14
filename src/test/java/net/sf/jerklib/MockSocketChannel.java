aba699270c8decf2256d1725e95ea8cea0c72a87
Eric Internicola <intere@gmail.com>
2015-11-14 12:44:21 -0700

    Fixed broken test class.

--- src/test/java/net/sf/jerklib/MockSocketChannel.java
+++ src/test/java/net/sf/jerklib/MockSocketChannel.java
@@ -3,9 +3,11 @@ package net.sf.jerklib;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketOption;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
 
 public class MockSocketChannel extends SocketChannel
 {
@@ -14,6 +16,34 @@ public class MockSocketChannel extends SocketChannel
 	protected MockSocketChannel(SelectorProvider provider)
 	{
 		super(provider);
+	}
+
+	@Override
+	public SocketChannel bind(SocketAddress local) throws IOException {
+		return null;
+	}
+
+	@Override
+	public <T> SocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+		return null;
+	}
+
+	public <T> T getOption(SocketOption<T> name) throws IOException {
+		return null;
+	}
+
+	public Set<SocketOption<?>> supportedOptions() {
+		return null;
+	}
+
+	@Override
+	public SocketChannel shutdownInput() throws IOException {
+		return null;
+	}
+
+	@Override
+	public SocketChannel shutdownOutput() throws IOException {
+		return null;
 	}
 
 	@Override
@@ -27,6 +57,11 @@ public class MockSocketChannel extends SocketChannel
 	public boolean finishConnect() throws IOException
 	{
 		return true;
+	}
+
+	@Override
+	public SocketAddress getRemoteAddress() throws IOException {
+		return null;
 	}
 
 	@Override
@@ -75,6 +110,11 @@ public class MockSocketChannel extends SocketChannel
 	{
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public SocketAddress getLocalAddress() throws IOException {
+		return null;
 	}
 
 	@Override
